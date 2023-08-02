aa7479d90deb19c70bf972f05ba04fbc1e0a23e5
Puru <5674762+tuladhar@users.noreply.github.com>
2023-08-02 18:37:45 +0545

    Update teleportclient.go

--- internal/pkg/teleportclient/teleportclient.go
+++ internal/pkg/teleportclient/teleportclient.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/giantswarm/microerror"
 	tc "github.com/gravitational/teleport/api/client"
 	tt "github.com/gravitational/teleport/api/types"
 	corev1 "k8s.io/api/core/v1"
@@ -31,15 +30,13 @@ func New(namespace string) (*TeleportClient, error) {
 	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
 	if err != nil {
-		fmt.Println("unable to get config to talk to the apiserver:", err)
-		return nil, err
+		return nil, fmt.Errorf("unable to get config to talk to the apiserver: %s", err)
 	}
 
 	// Create a new client
 	c, err := client.New(cfg, client.Options{})
 	if err != nil {
-		fmt.Println("unable to create a new client:", err)
-		return nil, err
+		return nil, fmt.Errorf("unable to create a new client: %s", err)
 	}
 
 	// Check if the Secret exists
@@ -52,27 +49,44 @@ func New(namespace string) (*TeleportClient, error) {
 		return nil, err
 	}
 
-	proxyAddrBytes, proxyAddrOk := secret.Data["proxyAddr"]
-	identityFileBytes, identityFileOk := secret.Data["identityFile"]
-	managementClusterNameBytes, managementClusterNameOk := secret.Data["managementClusterName"]
-	teleportVersionBytes, teleportVersionOk := secret.Data["teleportVersion"]
-	appNameBytes, appNameOk := secret.Data["appName"]
-	appVersionBytes, appVersionOk := secret.Data["appVersion"]
-	appCatalogBytes, appCatalogOk := secret.Data["appCatalog"]
-	if !proxyAddrOk && !identityFileOk && !managementClusterNameOk && !teleportVersionOk && appNameOk && appVersionOk && appCatalogOk {
-		return nil, fmt.Errorf("malformed Secret: required keys not found")
+	proxyAddr, err := getSecretString(secret, "proxyAddr")
+	if err != nil {
+		return nil, err
 	}
-	identityFile := string(identityFileBytes)
-	proxyAddr := string(proxyAddrBytes)
-	managementClusterName := string(managementClusterNameBytes)
-	teleportVersion := string(teleportVersionBytes)
-	appName := string(appNameBytes)
-	appVersion := string(appVersionBytes)
-	appCatalog := string(appCatalogBytes)
+
+	identityFile, err := getSecretString(secret, "identityFile")
+	if err != nil {
+		return nil, err
+	}
+
+	managementClusterName, err := getSecretString(secret, "managementClusterName")
+	if err != nil {
+		return nil, err
+	}
+
+	teleportVersion, err := getSecretString(secret, "teleportVersion")
+	if err != nil {
+		return nil, err
+	}
+
+	appName, err := getSecretString(secret, "appName")
+	if err != nil {
+		return nil, err
+	}
+
+	appVersion, err := getSecretString(secret, "appVersion")
+	if err != nil {
+		return nil, err
+	}
+
+	appCatalog, err := getSecretString(secret, "appCatalog")
+	if err != nil {
+		return nil, err
+	}
 
 	client, err := getClient(context.TODO(), proxyAddr, identityFile)
 	if err != nil {
-		return nil, microerror.Mask(err)
+		return nil, err
 	}
 
 	return &TeleportClient{
@@ -98,12 +112,12 @@ func getClient(ctx context.Context, proxyAddr, identityFile string) (*tc.Client,
 	})
 
 	if err != nil {
-		return nil, microerror.Mask(err)
+		return nil, err
 	}
 
 	_, err = c.Ping(ctx)
 	if err != nil {
-		return nil, microerror.Mask(err)
+		return nil, err
 	}
 
 	return c, nil
@@ -113,7 +127,7 @@ func (t *TeleportClient) GetToken(ctx context.Context, registerName string) (str
 	// Look for an existing token or generate one if it's expired
 	tokens, err := t.Client.GetTokens(ctx)
 	if err != nil {
-		return "", microerror.Mask(err)
+		return "", err
 	}
 
 	for _, t := range tokens {
@@ -127,7 +141,7 @@ func (t *TeleportClient) GetToken(ctx context.Context, registerName string) (str
 	token := randSeq(32)
 	newToken, err := tt.NewProvisionToken(token, []tt.SystemRole{tt.RoleKube, tt.RoleNode}, expiration)
 	if err != nil {
-		return "", microerror.Mask(err)
+		return "", err
 	}
 	metadata := newToken.GetMetadata()
 	metadata.Labels = map[string]string{
@@ -136,7 +150,7 @@ func (t *TeleportClient) GetToken(ctx context.Context, registerName string) (str
 	newToken.SetMetadata(metadata)
 	err = t.Client.UpsertToken(ctx, newToken)
 	if err != nil {
-		return "", microerror.Mask(err)
+		return "", err
 	}
 
 	return token, nil
@@ -146,7 +160,7 @@ func (t *TeleportClient) IsTokenValid(ctx context.Context, oldToken string, regi
 	{
 		tokens, err := t.Client.GetTokens(ctx)
 		if err != nil {
-			return false, microerror.Mask(err)
+			return false, err
 		}
 
 		for _, t := range tokens {
@@ -164,7 +178,7 @@ func (t *TeleportClient) IsTokenValid(ctx context.Context, oldToken string, regi
 func (t *TeleportClient) IsClusterRegistered(ctx context.Context, registerName string) (bool, tt.KubeServer, error) {
 	ks, err := t.Client.GetKubernetesServers(ctx)
 	if err != nil {
-		return false, nil, microerror.Mask(err)
+		return false, nil, err
 	}
 
 	for _, k := range ks {
@@ -178,8 +192,16 @@ func (t *TeleportClient) IsClusterRegistered(ctx context.Context, registerName s
 
 func (t *TeleportClient) DeregisterCluster(ctx context.Context, ks tt.KubeServer) error {
 	if err := t.Client.DeleteKubernetesServer(ctx, ks.GetHostID(), ks.GetCluster().GetName()); err != nil {
-		return microerror.Mask(err)
+		return err
 	}
 
 	return nil
+}
+
+func getSecretString(secret *corev1.Secret, key string) (string, error) {
+	b, ok := secret.Data[key]
+	if !ok {
+		return "", fmt.Errorf("malformed Secret: required key %q not found", key)
+	}
+	return string(b), nil
 }
