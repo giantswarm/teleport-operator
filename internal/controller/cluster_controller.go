bd52736a5ad345ccfc7e7a64b05c88007fa0e389
Spyros Synodinos <spyros@giantswarm.io>
2025-10-21 15:27:04 +0300

    remove kube agent cleanup methods and switch labels

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -50,9 +50,9 @@ type ClusterReconciler struct {
 //+kubebuilder:rbac:groups=cluster.x-k8s.io.giantswarm.io,resources=clusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=cluster.x-k8s.io.giantswarm.io,resources=clusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cluster.x-k8s.io.giantswarm.io,resources=clusters/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;delete
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;create;update;patch;delete
+// NOTE: Pod permissions removed - operator runs in management cluster and cannot access workload cluster pods
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -214,22 +214,6 @@ func (r *ClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 			return ctrl.Result{}, microerror.Mask(err)
 		}
 		log.Info("Created new config map with teleport join token", "configMapName", key.GetConfigmapName(cluster.Name, r.Teleport.Config.AppName), "roles", roles)
-
-		// Clean up teleport-kube-agent state secrets and restart pods when new configuration is created
-		log.Info("New configuration created, cleaning up teleport-kube-agent state and restarting pods")
-		if err := r.Teleport.DeleteTeleportKubeAgentStateSecrets(ctx, log, r.Client); err != nil {
-			log.Error(err, "Failed to delete teleport-kube-agent state secrets")
-			// Don't fail the reconciliation, just log the error and continue
-		}
-
-		// Wait 10 seconds between state deletion and pod restart to allow cleanup to propagate
-		log.Info("Waiting 10 seconds before restarting pods to allow state cleanup to propagate")
-		time.Sleep(10 * time.Second)
-
-		if err := r.Teleport.RestartTeleportKubeAgentPods(ctx, log, r.Client); err != nil {
-			log.Error(err, "Failed to restart teleport-kube-agent pods")
-			// Don't fail the reconciliation, just log the error and continue
-		}
 	} else {
 		token, err := r.Teleport.GetTokenFromConfigMap(ctx, configMap)
 		if err != nil {
@@ -249,21 +233,6 @@ func (r *ClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 			}
 			log.Info("Updated config map with new teleport join token", "configMapName", configMap.GetName(), "roles", roles)
 
-			// Clean up teleport-kube-agent state secrets and restart pods when configuration is updated
-			log.Info("Configuration updated, cleaning up teleport-kube-agent state and restarting pods")
-			if err := r.Teleport.DeleteTeleportKubeAgentStateSecrets(ctx, log, r.Client); err != nil {
-				log.Error(err, "Failed to delete teleport-kube-agent state secrets")
-				// Don't fail the reconciliation, just log the error and continue
-			}
-
-			// Wait 10 seconds between state deletion and pod restart to allow cleanup to propagate
-			log.Info("Waiting 10 seconds before restarting pods to allow state cleanup to propagate")
-			time.Sleep(10 * time.Second)
-
-			if err := r.Teleport.RestartTeleportKubeAgentPods(ctx, log, r.Client); err != nil {
-				log.Error(err, "Failed to restart teleport-kube-agent pods")
-				// Don't fail the reconciliation, just log the error and continue
-			}
 		} else {
 			log.Info("ConfigMap has valid teleport join token", "configMapName", configMap.GetName(), "roles", roles)
 		}
